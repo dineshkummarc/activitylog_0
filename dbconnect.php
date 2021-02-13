ee36747b0f2283097a0d5cfedae21cfbdfb80531
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-13 23:46:54 +0530

    update

--- dbconnect.php
+++ dbconnect.php
@@ -6,11 +6,12 @@ $username = "root";
 $password = "";
 $dbname = "activitylog";
 
+try {
   $conn = mysqli_connect($servername, $username, $password, $dbname);
   // set the PDO error mode to exception
   // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
 // } catch(PDOException $e) {
   // echo "Connection failed: " . $e->getMessage();
-
+}
 ?>
